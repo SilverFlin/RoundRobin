5088d640a6ed017f2c8bc68e4b844ea2795c4f69
Sosnicus <104942646+Sosnicus@users.noreply.github.com>
2022-05-04 15:06:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,22 @@ conastructor(numero,tiempo,tamano,estado)
         this.tiempo = tiempo;
         this.tamano = tamano;
         this.estado = estado;
+    
+        getSEstado()
+    {
+        switch(estado)
+        {
+            case 0:
+                return "";
+            case 1:
+                return "En espera";
+            case 2:
+                return "Asignado";
+            case 3:
+                return "Terminado";
+        }
+        return "";
     }
+    
+  }
 }
