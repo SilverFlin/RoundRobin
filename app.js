701c8816e2dac14a7a33eb26955aa99c0cbf6e91
Sosnicus <104942646+Sosnicus@users.noreply.github.com>
2022-05-04 14:55:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-
+class Memoria 
+{
+  
+ 
+  constructor( bloque,  tamaño,  proceso,  tiempo) 
+  {
+        this.bloque = bloque;
+        this.tamaño = tamaño;
+        this.proceso = proceso;
+        this.tiempo = tiempo;
+    
+  }
+  
+}
