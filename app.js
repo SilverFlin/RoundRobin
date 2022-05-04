ceb6f1b6d6ce2b13495e1bd2c941321e0100472f
Sosnicus <104942646+Sosnicus@users.noreply.github.com>
2022-05-04 16:04:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ class Proceso
         this.tiempo =  [1,1,4,6,10,2,3,7,2,7,5,8,10,8,3,6,4,8,4,8,5,5,7,6,10];
         this.tamano =  [760,3210,4190,5760,9350,1380,5950,2030,2550,420,3930,8940,6890,9140,220,6580,740,3610,6990,3820,3290,2710,7540,7540,8390];
         this.tarea= [];
+        this.memorias= [];
+        this.mmemorias= [];
         for(let cont=0; cont<25; cont++)
         {
             tarea[cont] = new this.tarea(cont+1, this.tiempo[cont], this.tamano[cont], 0);
