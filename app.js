cd32dab04f1865a29914c39f5a6ce20bfef6af2a
Sosnicus <104942646+Sosnicus@users.noreply.github.com>
2022-05-04 15:36:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,24 @@ class Tarea {
     return "";
   };
 }
+class Proceso 
+{
+  
+  
+  constructor() 
+  {
+        const QUANTUM = 5;
+        memoria= 50000;
+        numBloque = 1;
+        empi=0;
+        const tiempo =  [1,1,4,6,10,2,3,7,2,7,5,8,10,8,3,6,4,8,4,8,5,5,7,6,10];
+        const tamano =  [760,3210,4190,5760,9350,1380,5950,2030,2550,420,3930,8940,6890,9140,220,6580,740,3610,6990,3820,3290,2710,7540,7540,8390];
+        const tarea= [];
+        for(cont=0; cont<25; cont++)
+        {
+            tarea[cont] = new Tarea(cont+1, tiempo[cont], tamano[cont], 0);
+        }
+    
+  }
+  
+}
