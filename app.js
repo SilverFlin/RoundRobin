123eeba7ee3aed672a95a7180fbf79acc5907c40
Sosnicus <104942646+Sosnicus@users.noreply.github.com>
2022-05-05 15:46:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ isProcesado() {
     getTareaM(num){
         let cont=0;
         for(let i= 0; i <this.tarea.lenght; i++){
-            if(this.numero() === num){
+            if(this.numero === num){
                 return this.tarea[i];
             }
         }
@@ -118,7 +118,7 @@ isProcesado() {
   getMemoriaT(num){
         for(let i= 0; i <this.memorias.lenght; i++)
         {
-            if(this.bloque() === num)
+            if(this.bloque === num)
             {
                 return this.memorias;
             }
